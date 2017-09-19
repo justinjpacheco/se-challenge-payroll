0b0edfee93d7bb5aa4bf466dfc9d58d75d82e8a0
Justin Pacheco <justin.pacheco@breqwatr.com>
2017-09-19 02:33:49 -0400

    updated to use latest lib path

--- src/server/bin/sync-db.py
+++ src/server/bin/sync-db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from payroll_schema import Base, JobGroups
+from payrolllib.schema import Base, JobGroups
 
 engine = create_engine('mysql://payroll:password@localhost/payroll')
 Base.metadata.create_all(engine)
